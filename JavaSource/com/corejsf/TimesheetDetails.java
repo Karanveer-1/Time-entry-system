5f24670ff27932daa2888e1467a586faf5081a2d
Karanveer Singh Khanna <kayvie2@gmail.com>
2018-10-19 21:53:09 -0700

    Added timesheet logic. User can now create there own timesheets and add projects in them.

--- JavaSource/com/corejsf/TimesheetDetails.java
+++ JavaSource/com/corejsf/TimesheetDetails.java
@@ -1,5 +1,9 @@
 package com.corejsf;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
@@ -11,34 +15,95 @@ import com.corejsf.timesheet.data.TimesheetData;
 import ca.bcit.infosys.employee.Employee;
 import ca.bcit.infosys.timesheet.Timesheet;
 import ca.bcit.infosys.timesheet.TimesheetCollection;
+import ca.bcit.infosys.timesheet.TimesheetRow;
 
 @Named
 @RequestScoped
 public class TimesheetDetails implements TimesheetCollection {
     @Inject TimesheetData timesheetData;
+    @Inject EmployeeDetails emp;
     
     @Override
     public List<Timesheet> getTimesheets() {
-        // TODO Auto-generated method stub
-        return null;
+        return timesheetData.getTimesheetList();
     }
 
     @Override
     public List<Timesheet> getTimesheets(Employee e) {
-        // TODO Auto-generated method stub
-        return null;
+        ArrayList<Timesheet> allTimesheets = new ArrayList<Timesheet>();
+        for (Timesheet ts: getTimesheets()) {
+            if (ts.getEmployee().getEmpNumber() == e.getEmpNumber()) {
+                allTimesheets.add(ts);
+            }
+        }
+        return allTimesheets;
+    }
+    
+    public boolean hasCurrentTimesheet(Employee e) {
+//        Calendar c = new GregorianCalendar();
+//        int currentDay = c.get(Calendar.DAY_OF_WEEK);        what does it do?????
+//        int leftDays = Calendar.FRIDAY - currentDay;
+//        c.add(Calendar.DATE, leftDays);
+//        Date endWeek = c.getTime();
+        
+        for (Timesheet ts: timesheetData.getTimesheetList()) {
+            if (ts.getEmployee().getEmpNumber() == e.getEmpNumber()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public Timesheet getCurrentTimesheet(Employee e) {
-        // TODO Auto-generated method stub
+        for (Timesheet ts: getTimesheets()) {
+            if (ts.getEmployee().getEmpNumber() == e.getEmpNumber()) {
+                return ts;
+            }
+        }
         return null;
     }
 
     @Override
     public String addTimesheet() {
-        // TODO Auto-generated method stub
-        return null;
+        Timesheet temp = new Timesheet();
+        
+        ArrayList<TimesheetRow> tempRows = (ArrayList<TimesheetRow>) temp.getDetails();
+        
+        for(int i = 0; i < 5;i++) {
+            tempRows.add(new TimesheetRow());
+        }
+        
+        temp.setEmployee(emp.getCurrentEmployee());
+        timesheetData.getTimesheetList().add(temp);
+        
+        System.out.println("success");
+        
+        return "editTimesheet?faces-redirect=true";
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JavaSource/com/corejsf/timesheet/data/TimesheetData.java
+++ JavaSource/com/corejsf/timesheet/data/TimesheetData.java
@@ -1,20 +1,22 @@
 package com.corejsf.timesheet.data;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
 import ca.bcit.infosys.timesheet.Timesheet;
-import ca.bcit.infosys.timesheet.TimesheetRow;
 
 @Named
 @ApplicationScoped
 public class TimesheetData {
+    
     ArrayList<Timesheet> timesheetList = new ArrayList<Timesheet>();
     
-    ArrayList<TimesheetRow> timesheetrowList = new ArrayList<TimesheetRow>(Arrays.asList());
-    
+    public ArrayList<Timesheet> getTimesheetList() {
+        return timesheetList;
+    }
+
+    public void setTimesheetList(ArrayList<Timesheet> timesheetList) {
+        this.timesheetList = timesheetList;
+    }    
 }
